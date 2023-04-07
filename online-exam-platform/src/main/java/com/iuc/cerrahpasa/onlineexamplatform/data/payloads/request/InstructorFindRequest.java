b364a7cd09a2c8744064154d7823f11a7bd7c28e
bunuox <bunyamin.klcr@outlook.com>
2023-04-07 14:17:52 +0300

    instructor api created

---
+++ online-exam-platform/src/main/java/com/iuc/cerrahpasa/onlineexamplatform/data/payloads/request/InstructorFindRequest.java
@@ -0,0 +1,19 @@
+package com.iuc.cerrahpasa.onlineexamplatform.data.payloads.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InstructorFindRequest {
+    @NotBlank
+    @NotNull
+    private String email;
+}
