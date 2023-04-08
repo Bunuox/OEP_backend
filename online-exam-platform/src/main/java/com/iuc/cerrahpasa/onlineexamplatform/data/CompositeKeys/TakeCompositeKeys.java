db366eebfd886eb6cd9ef2f9f8ae78439fae69c6
bunuox <bunyamin.klcr@outlook.com>
2023-04-08 23:58:29 +0300

    take api created

---
+++ online-exam-platform/src/main/java/com/iuc/cerrahpasa/onlineexamplatform/data/CompositeKeys/TakeCompositeKeys.java
@@ -0,0 +1,9 @@
+package com.iuc.cerrahpasa.onlineexamplatform.data.CompositeKeys;
+
+import java.io.Serializable;
+
+public class TakeCompositeKeys implements Serializable {
+    private Long courseId;
+    private Long studentId;
+    private Long instructorId;
+}
---
+++ online-exam-platform/src/main/java/com/iuc/cerrahpasa/onlineexamplatform/repository/TakeRepository.java
@@ -0,0 +1,7 @@
+package com.iuc.cerrahpasa.onlineexamplatform.repository;
+
+import com.iuc.cerrahpasa.onlineexamplatform.data.model.Take;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TakeRepository extends JpaRepository<Take, Long> {
+}
